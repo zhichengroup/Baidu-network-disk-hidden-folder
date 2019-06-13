f5158f2734ff16aac6dbb8d552dce5f4039e43b0
zhichengroup <zhichengroup@gmail.com>
2019-06-13 09:46:14 +0800

    Update Baidu-network-disk-hidden-folder-greasyfork-Tampermonkey.js

--- Baidu-network-disk-hidden-folder-greasyfork-Tampermonkey.js
+++ Baidu-network-disk-hidden-folder-greasyfork-Tampermonkey.js
@@ -1,44 +1,60 @@
 // ==UserScript==
-// @name         百度网盘网页版隐藏“我的卡包”和“我的应用数据”
+// @name         百度网盘网页版隐藏“我的卡包”、“我的应用数据”和“已购资源”   2019-06-13 更新
 // @namespace    https://github.com/zhichengroup/Baidu-network-disk-hidden-folder
-// @version      0.9.0 beta
-// @description  百度网盘网页版隐藏“我的卡包”和“我的应用数据” 2018-09-27 更新
+// @version      0.9.11 beta
+// @description  百度网盘网页版隐藏“我的卡包”、“我的应用数据”和“已购资源”   2019-06-13 更新
 // @author       zhichengroup
 // @match        *://pan.baidu.com/*
 // @include      *://pan.baidu.com/*
-// @require      https://code.jquery.com/jquery-1.12.4.min.js
-// @update       2018.09.27-v0.9.0 beta 代码重写。返回上一级、按文件名、大小、日期排序时存在bug，解决方案：在顶栏晃一晃鼠标就没有了。
+// @require      https:code.jquery.com/jquery-1.12.4.min.js
+
+/*
+# 百度网盘网页版隐藏“我的卡包”、“我的应用数据”和“已购资源” 2019-06-13 更新
+## 2019-06-13 更新
+## pan.baidu.com
+### 日期
+2019-06-13
+### 注意
+此脚本只能在网页版隐藏“我的卡包”、“我的应用数据”和“已购资源”，并不能实际删除！
+### 联系方式
+zhichengroup@gmail.com
+*/
 
 // ==/UserScript==
 
-(function () {
-    'use strict';
+(function() {
+	'use strict';
 
-    $(document).ready(function(){
+	function clearfunc() {
+        $("dd:first").hide();
+        $("dd.open-enable.AuPKyz.g-clearfix:nth-of-type(1)").hide();
+		$("dd.open-enable.AuPKyz.g-clearfix:nth-of-type(2)").hide();
+        //$("[title='已购资源']").parent().parent().parent().hide(); // .remove();
+		//$("[title='我的卡包']").parent().parent().parent().hide(); // .remove();
+		$("[title='我的应用数据']").parent().parent().parent().hide(); // .remove();
+        //console.log("Baiduwangpan_script_hide_func");
+	};
 
-        function clearfunc(){
-            $("[title='我的卡包']").parent().parent().parent().hide(); // .remove();
-            $("[title='我的应用数据']").parent().parent().parent().hide(); // .remove();
-        }
+	$("*").click(function() {
+		clearfunc();
+	});
 
-        clearfunc();
+	$("*").dblclick(function() {
+		clearfunc();
+	});
 
-        $("body").click(function(){
-            clearfunc();
-        });
+	$("*").mouseenter(function() {
+		clearfunc();
+	});
 
-        $("body").dblclick(function(){
-            clearfunc();
-        });
+	$("*").mouseleave(function() {
+		clearfunc();
+	});
 
-        $("*").mouseenter(function(){
-            clearfunc();
-        });
-
-        $("*").mouseleave(function(){
-            clearfunc();
-        });
-
-    });
+	$(document).ready(function() {
+		clearfunc();
+	});
+	clearfunc();
+	console.log("Baiduwangpan_script_hide_func_success");
 
 })();
