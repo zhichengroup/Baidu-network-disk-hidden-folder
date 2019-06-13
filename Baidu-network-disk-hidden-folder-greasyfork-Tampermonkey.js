635847d968a90eb8e3b1ad8159d04469f176f895
zhichengroup <zhichengroup@gmail.com>
2019-06-13 09:50:41 +0800

    Update Baidu-network-disk-hidden-folder-greasyfork-Tampermonkey.js
    0.9.11 beta 增加隐藏”已购资源“。

--- Baidu-network-disk-hidden-folder-greasyfork-Tampermonkey.js
+++ Baidu-network-disk-hidden-folder-greasyfork-Tampermonkey.js
@@ -9,15 +9,20 @@
 // @require      https:code.jquery.com/jquery-1.12.4.min.js
 
 /*
-# 百度网盘网页版隐藏“我的卡包”、“我的应用数据”和“已购资源” 2019-06-13 更新
-## 2019-06-13 更新
-## pan.baidu.com
-### 日期
-2019-06-13
+# 百度网盘网页版隐藏“我的卡包”、“我的应用数据”和“已购资源”
+### Baidu-network-disk-hidden-folder
+### Tampermonkey GreasyFork
+### 2019-06-13 更新
+### pan.baidu.com
 ### 注意
-此脚本只能在网页版隐藏“我的卡包”、“我的应用数据”和“已购资源”，并不能实际删除！
+此脚本只能在网页版隐藏“我的卡包”和“我的应用数据”，并不能实际删除！！！
+### 无关脚本的话
+“我的应用数据”文件夹是可以通过接口删除的。
+理论上如果不用百度系app（比如百度输入法，百度浏览器等，我不知道“我的应用数据”文件夹是由哪个百度系app触发的）是不会再生成的。
+目前正在测试，云端删除“我的应用数据”文件夹没有再自动恢复过，“我的卡包”文件夹删除。
 ### 联系方式
-zhichengroup@gmail.com
+如果对这个脚本有兴趣，或者想尝试我的思路，脚本失效或者好久没更新请联系我，我会尽量完善。
+zhichengroup@gmail.com   [greasyfork](https://greasyfork.org/scripts/371821)   [github](https://github.com/zhichengroup/Baidu-network-disk-hidden-folder)
 */
 
 // ==/UserScript==
